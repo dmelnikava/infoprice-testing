389d2e1052336442e37ebe41e5a2d1da80d8ae6a
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-01-09 16:12:33 +0300

    Befor and after methods were added to the InfopriceTest

--- src/test/java/com/solvd/infopricetesting/InfopriceTest.java
+++ src/test/java/com/solvd/infopricetesting/InfopriceTest.java
@@ -4,25 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 import java.util.concurrent.TimeUnit;
 
 public class InfopriceTest {
 
     private WebDriver driver;
+    private HomePage homePage;
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "/Users/solvd/Documents/selenium/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://infoprice.by/");
+        homePage = new HomePage(driver);
     }
 
     @Test(testName = "Check returning from the Basket page to the Home page")
     public void checkReturnFromBasketPageToHomePageTest() {
-        HomePage homePage = new HomePage(driver);
         BasketPage basketPage = homePage.clickBasketIcon();
         basketPage.clickOnHomePageButton();
 
@@ -31,16 +32,19 @@ public class InfopriceTest {
 
     @Test(testName = "Check if the Filter menu locates under Catalog menu")
     public void checkFilterMenuLocationTest() {
-        HomePage homePage = new HomePage(driver);
         Assert.assertNotNull(homePage.getFilterButtonLocation());
     }
 
     @Test(testName = "Check the opening of the Filter menu by clicking on the text")
     public void checkOpenFilterMenuTest() {
-        HomePage homePage = new HomePage(driver);
         homePage.clickFilterButton();
         WebElement filterStatusBar = homePage.getFilterBarShowStatus();
 
         Assert.assertEquals(filterStatusBar.getAttribute("class"), "collapse show");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
